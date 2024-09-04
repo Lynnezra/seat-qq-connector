b4ebd0c3367de9eaed118259209ca4d628121d64
FeiBam <feibam19690104@gmail.com>
2024-09-04 21:29:15 +0900

    修复无法绑定BUG 重修整理逻辑

--- src/Http/Controllers/RegistrationController.php
+++ src/Http/Controllers/RegistrationController.php
@@ -51,14 +51,59 @@ class RegistrationController extends Controller{
 
     public function handlerSubmit(Request $request) {
         $settings = setting('seat-connector.drivers.QQ', true);
-        $allow_modification = $settings -> allow_modification_bind_infomation;
+        $allow_modification = $settings->allow_modification_bind_infomation;
         $seat_user = auth()->user();
-
-        if($allow_modification == 0 || $allow_modification == False){
-            return redirect()->back()
-            ->with('warning', trans("seat-qq-connector::seat.not_allow_modification_bind_infomation_waring"));
+        $driver_user = User::where('connector_type', 'qq')
+            ->where('user_id', $seat_user->id)
+            ->first();
+    
+        if (!is_null($driver_user)) {
+            if (!$allow_modification) {
+                return redirect()->back()
+                    ->with('warning', trans("seat-qq-connector::seat.not_allow_modification_bind_infomation_waring"));
+            }
+            // 调用更新绑定信息的方法
+            return $this->handlerUpdateBindInfomation($request);
         }
+    
+        // 调用绑定 QQ 账户的方法
+        return $this->handlerBindQQAccount($request);
+    }
 
+    public function handlerBindQQAccount(Request $request) {
+        $validatedData = $request->validate([
+            'qq_number' => [
+                'required',  
+                'integer',
+                'digits_between:5,18',
+            ],
+            'qq_name' => [
+                'required', 
+                'string',
+                'min:1',
+                'max:32',
+            ],
+        ]);
+    
+        $driver_user = new User();
+        $seat_user = auth()->user();
+        $qq_number = $validatedData['qq_number'];
+        $qq_name = $validatedData['qq_name'];
+        $seat_user_id = $seat_user->id;
+        $unique_id = md5("$seat_user_id$qq_name$qq_number");
+    
+        $driver_user->connector_type = 'qq';
+        $driver_user->user_id = $seat_user_id;
+        $driver_user->connector_id = $qq_number;
+        $driver_user->unique_id = $unique_id;
+        $driver_user->connector_name = $qq_name;
+    
+        $driver_user->save();
+    
+        return redirect(route("seat-connector.identities"));
+    }
+
+    public function handlerUpdateBindInfomation(Request $request) {
         $validatedData = $request->validate([
             'qq_number' => [
                 'required_without:qq_name',
@@ -72,28 +117,20 @@ class RegistrationController extends Controller{
                 'max:32',
             ],
         ]);
-
-        $qq_number = $validatedData['qq_number'];
-        $qq_name = $validatedData['qq_name'];
-        $seat_user_id = $seat_user -> id;
-        $unique_id = md5("$seat_user_id$qq_name$qq_number");
-
+    
+        $seat_user = auth()->user();  // 先获取 $seat_user
         $driver_user = User::where('connector_type', 'qq')
-        ->where('user_id', $seat_user->id)
-        ->first();
-
-        if(!is_null($driver_user)){
-            $unique_id = $driver_user->unique_id;
-        }
-
-        $driver_user = User::updateOrCreate([
-            'connector_type' => 'qq',
-            'user_id'        => auth()->user()->id,
-        ], [
-            'connector_id'   => "$qq_number",
-            'unique_id'      => "$unique_id",
-            'connector_name' => "$qq_name",
-        ]);
+            ->where('user_id', $seat_user->id)
+            ->first();
+    
+        $qq_number = $validatedData['qq_number'] ?? $driver_user->connector_id;
+        $qq_name = $validatedData['qq_name'] ?? $driver_user->connector_name;
+    
+        $driver_user->connector_id = $qq_number;
+        $driver_user->connector_name = $qq_name;
+    
+        $driver_user->update();
+    
         return redirect(route("seat-connector.identities"));
     }
 }
